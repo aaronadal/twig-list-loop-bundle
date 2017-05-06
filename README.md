065fd3c68980f4141fa413fb96f22629adc39eb1
Aarón Nadal <aaronadal.dev@gmail.com>
2017-05-07 01:02:38 +0200

    First project import

---
+++ README.md
@@ -0,0 +1,3 @@
+# List Twig Tag Bundle
+
+Symfony integration for the aaronadal/list-twig-tag library.
---
+++ src/AaronadalTwigListLoopBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Aaronadal\TwigListLoopBundle;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Aarón Nadal <aaronadal.dev@gmail.com>
+ */
+class AaronadalTwigListLoopBundle extends Bundle
+{
+
+}
